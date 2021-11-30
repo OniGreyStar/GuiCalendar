7e1cccd290a5baa495007d239baf546d6064d5c8
Leon <Leon@DESKTOP-F34HQ1D>
2021-11-29 23:21:41 -0800

    First Commit

---
+++ src/cal/CalendarTester.java
@@ -0,0 +1,15 @@
+package cal;
+/**
+ * 
+ * @author Leon, Chirag
+ * @Version 1.0
+ * @Since 8/4/2021
+ *
+ */
+public class CalendarTester {
+	public static void main(String[] args)  {
+		CalendarModel model = new CalendarModel();
+		View view = new View(model);
+		model.attach(view);
+	}
+}
